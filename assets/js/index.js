92fa863654218b291a8761a4d332bf5aed905eae
matgus217 <>
2023-12-07 06:59:35 +0000

    Add link between index.html and js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,7 @@
 /* jshint esversion: 11 */
+
+const gameBoard = document.querySelector(.game-board);
+const scoreElement = document.querySelector(".score");
+const highScoreElement = document.querySelector(".high-score");
+const controls = document.querySelectorAll(".controls i");
+
