a5c0126ce274e783b0a595058b9a1a84e7183d80
matgus217 <>
2023-12-08 12:32:49 +0000

    Add styles and divs

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,11 @@
-/* jshint esversion: 11 */
+let dom_playAagain = document.querySelector("#play-again");
+let dom_yourP = document.querySelector("#your-p");
+let dom_points = document.createElement("points");
+document.querySelector("#points").appendChild(dom_points);
+let CTX = dom_points.getContext("2d");
 
-const gameBoard = document.querySelector(.game-board);
-const scoreElement = document.querySelector(".score");
-const highScoreElement = document.querySelector(".high-score");
-const controls = document.querySelectorAll(".controls i");
+
+const W = (dom_points.width = 500);
+const H = (dom_points.height = 500);
+
 
