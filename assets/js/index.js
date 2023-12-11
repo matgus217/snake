edb6cfc81c0201ccefdfe6622e9a0c26860af7da
matgus217 <>
2023-12-11 07:38:22 +0000

    Add and change styles,names and color

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,1 @@
-let dom_playAagain = document.querySelector("#play-again");
-let dom_yourP = document.querySelector("#your-p");
-let dom_points = document.createElement("points");
-document.querySelector("#points").appendChild(dom_points);
-let CTX = dom_points.getContext("2d");
-
-
-const W = (dom_points.width = 500);
-const H = (dom_points.height = 500);
-
 
